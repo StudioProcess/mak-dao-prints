64c770a0d68ce36c2a6e213ada5aaafbb3ae7f99
Martin Grödl <martin@process.studio>
2023-12-05 14:22:23 +0100

    Add hotkeys

--- app/sketch.js
+++ app/sketch.js
@@ -21,4 +21,22 @@ function draw() {
     // console.log(frameCount);
 }
 
+document.addEventListener('keydown', e => {
+    // console.log(e.key, e.keyCode, e);
+   
+    if (e.key == 'f') { // f .. fullscreen
+        util.toggle_fullscreen();
+    }
+    
+    else if (e.key == 's') { // s .. save frame
+        util.save_canvas( params );
+    }
+    
+    else if (e.key == 'h' || e.key == 'Tab') { // h or Tab .. toggle gui
+        gui.toggle();
+        e.preventDefault();
+    }
+
+});
+
 util.register_global({setup, draw});
--- libs/lil-gui-patch.js
+++ libs/lil-gui-patch.js
@@ -3,7 +3,7 @@ import * as lil from '../node_modules/lil-gui/dist/lil-gui.esm.min.js';
 // // Get name of Controller
 // // Adds a function getName() to the Controller prototype
 // dat.controllers.Controller.prototype.getName = function() {
-//   return this.__li.querySelector('.property-name').textContent;
+//     return this.__li.querySelector('.property-name').textContent;
 // }
 
 
@@ -25,25 +25,25 @@ lil.Controller.prototype.lock = function(lock = true) {
 // Adds a function get() to the GUI prototype
 // Takes one or more controller names, where the last one is the actual name of the controller and the preceding ones are names of (nested) folders the controller is in
 lil.GUI.prototype.get = function(...names) {
-  for (const [i, name] of names.entries()) {
-    if (i === names.length-1) { // the last path element
-      // find element
-      let el = this.controllers.find( c => c._name == name );
-      if (el !== undefined) { return el; }
-      el = this.folders.find( f => f._title == name ); // try folders instead
-      if (el === undefined) { 
-        throw { error: `GUI element not found: ${name}` };
-      }
-      return el;
+    for (const [i, name] of names.entries()) {
+        if (i === names.length-1) { // the last path element
+            // find element
+            let el = this.controllers.find( c => c._name == name );
+            if (el !== undefined) { return el; }
+            el = this.folders.find( f => f._title == name ); // try folders instead
+            if (el === undefined) { 
+                throw { error: `GUI element not found: ${name}` };
+            }
+            return el;
+        }
+        // find folder
+        let folder = this.folders.find( f => f._title == name );
+        if (folder === undefined) { 
+            throw { error: `GUI folder not found: ${name}` };
+        }
+        // recur
+        return folder.get(...names.slice(1));
     }
-    // find folder
-    let folder = this.folders.find( f => f._title == name );
-    if (folder === undefined) { 
-        throw { error: `GUI folder not found: ${name}` };
-    }
-    // recur
-    return folder.get(...names.slice(1));
-  }
 };
 
 // Add Controllers for all properties of an object
@@ -58,30 +58,30 @@ lil.GUI.prototype.get = function(...names) {
 //    this allows specifying min/max/step or options
 // object ... adds a folder and calls addAll on it again
 lil.GUI.prototype.addAll = function(obj) {
-  function is_hex_color(str) {
-    return /^#[0-9a-fA-F]{3}$/.test(str) || /^#[0-9a-fA-F]{6}$/.test(str);
-  }
-  for (const [k, v] of Object.entries(obj)) {
-    if (typeof v == 'string' && is_hex_color(v)) {
-      this.addColor(obj, k);
-    } else if (['number', 'string', 'function', 'boolean'].includes(typeof v) ) {
-      this.add(obj, k);
-    } else if (Array.isArray(v)) {
-      if (v.length == 0) continue;
-      obj[k] = v[0];
-      this.add(obj, k, ...v.slice(1));
-    } else if (typeof v == 'object') {
-      const f = this.addFolder(k); 
-      f.addAll(v);
+    function is_hex_color(str) {
+      return /^#[0-9a-fA-F]{3}$/.test(str) || /^#[0-9a-fA-F]{6}$/.test(str);
     }
-  }
+    for (const [k, v] of Object.entries(obj)) {
+        if (typeof v == 'string' && is_hex_color(v)) {
+            this.addColor(obj, k);
+        } else if (['number', 'string', 'function', 'boolean'].includes(typeof v) ) {
+            this.add(obj, k);
+        } else if (Array.isArray(v)) {
+            if (v.length == 0) continue;
+            obj[k] = v[0];
+            this.add(obj, k, ...v.slice(1));
+        } else if (typeof v == 'object') {
+            const f = this.addFolder(k); 
+            f.addAll(v);
+        }
+    }
 };
 
 // Update display of all controllers
 lil.GUI.prototype.updateDisplay = function(obj) {
-  for (let c of this.controllersRecursive()) {
-    c.updateDisplay();
-  }
+    for (let c of this.controllersRecursive()) {
+        c.updateDisplay();
+    }
 }
 
 // Shortcut for adding functions, without the need that the function be a property of an object
@@ -90,6 +90,7 @@ lil.GUI.prototype.addFunction = function(fn) {
     return this.add(obj, fn.name);
 };
 
+// Lock all controllers
 lil.GUI.prototype.lock = function(lock = true, label = ' [Locked]') {
     for (let c of this.controllersRecursive()) {
         c.lock(lock);
@@ -101,9 +102,19 @@ lil.GUI.prototype.lock = function(lock = true, label = ' [Locked]') {
     }
 };
 
+// Disable all controllers
 lil.GUI.prototype.disable = function(disable = true) {
     for (let c of this.controllersRecursive()) {
         c.disable(disable);
+    }
+};
+
+// Toggle GUI visibility
+lil.GUI.prototype.toggle = function(on = undefined) {
+    if (on !== undefined) {
+        this.show(on);
+    } else {
+        this.show(this._hidden);
     }
 };
 
